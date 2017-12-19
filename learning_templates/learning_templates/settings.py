90956a1208bd2efbd3989d53fe12f506f8065489
kamilioner <kamilbudka@gmail.com>
2017-12-19 11:36:42 +0100

    Update settings.py

--- learning_templates/learning_templates/settings.py
+++ learning_templates/learning_templates/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'qx97@%9hyw!&jx4+x+o90iij+brfb(m+e)_)6_&&2rz6qr^2*-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['renoilimak.pythonanywhere.com']
 
 
 # Application definition
